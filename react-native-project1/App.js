0338a5d86fabe9db8d3c4b54090c359dc752ff71
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-07-06 01:17:29 -0400

    setting up todo list

--- react-native-project1/App.js
+++ react-native-project1/App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [enteredGoalText, setEnteredGoalText] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	function goalInputHandler(enteredText) {
+		setEnteredGoalText(enteredText);
+	}
+
+	function addGoalHandler() {
+		setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
+	}
+
+	return (
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Your course goal"
+					onChangeText={goalInputHandler}
+				/>
+				<Button title="Add Goal" onPress={addGoalHandler} />
+			</View>
+			<View style={styles.goalsContainer}>
+				{courseGoals.map(goal => (
+					<View key={goal} style={styles.goalItem}>
+						<Text style={styles.goalText}>{goal}</Text>
+					</View>
+				))}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	appContainer: {
+		flex: 1,
+		paddingTop: 50,
+		paddingHorizontal: 16
+	},
+	inputContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: '#cccccc'
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: '#cccccc',
+		width: '70%',
+		marginRight: 8,
+		padding: 8
+	},
+	goalsContainer: {
+		flex: 5
+	},
+	goalItem: {
+		margin: 8,
+		padding: 8,
+		borderRadius: 6,
+		backgroundColor: '#5e0acc'
+	},
+	goalText: {
+		color: 'white'
+	}
 });
