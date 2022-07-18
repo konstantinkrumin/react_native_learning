7b430ca2dcd068f91d90bad9514e3e9f4ca908ab
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-07-17 23:38:56 -0400

    set up buttons styling and layout

--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -5,9 +5,21 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
 	return (
 		<View style={styles.inputContainer}>
-			<TextInput />
-			<PrimaryButton>Reset</PrimaryButton>
-			<PrimaryButton>Confirm</PrimaryButton>
+			<TextInput
+				style={styles.numberInput}
+				maxLength={2}
+				keyboardType="number-pad"
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
+			<View style={styles.buttonsContainer}>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton>Reset</PrimaryButton>
+				</View>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton>Confirm</PrimaryButton>
+				</View>
+			</View>
 		</View>
 	);
 }
@@ -16,10 +28,12 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
 	inputContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
 		marginTop: 100,
 		marginHorizontal: 24,
 		padding: 16,
-		backgroundColor: '#72063c',
+		backgroundColor: '#4e0329',
 		borderRadius: 8,
 		elevation: 4,
 		shadowColor: 'black',
@@ -29,5 +43,22 @@ const styles = StyleSheet.create({
 		},
 		shadowRadius: 6,
 		shadowOpacity: 0.25
+	},
+	numberInput: {
+		width: 50,
+		height: 50,
+		fontSize: 32,
+		borderBottomColor: '#ddb52f',
+		borderBottomWidth: 2,
+		color: '#ddb52f',
+		marginVertical: 8,
+		fontWeight: 'bold',
+		textAlign: 'center'
+	},
+	buttonsContainer: {
+		flexDirection: 'row'
+	},
+	buttonContainer: {
+		flex: 1
 	}
 });
