5157b4e2a093b225289e9673b1fc5f8920441a56
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-07-19 01:23:33 -0400

    set up logic for switching to the gamescreen

--- mini-game/App.js
+++ mini-game/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+	const [userNumber, setUserNumber] = useState();
+
+	function pickedNumberHandler(pickedNumber) {
+		setUserNumber(pickedNumber);
+	}
+
+	let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+	if (userNumber) {
+		screen = <GameScreen />;
+	}
+
 	return (
 		<LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
 			<ImageBackground
@@ -12,7 +26,7 @@ export default function App() {
 				style={styles.rootScreen}
 				imageStyle={styles.backgroundImage}
 			>
-				<StartGameScreen />
+				{screen}
 			</ImageBackground>
 		</LinearGradient>
 	);
--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -1,8 +1,32 @@
-import { TextInput, View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
+	const [enteredNumber, setEnteredNumber] = useState('');
+
+	function numberInputHandler(enteredText) {
+		setEnteredNumber(enteredText);
+	}
+
+	function resetInputHandler() {
+		setEnteredNumber('');
+	}
+
+	function confirmInputHandler() {
+		const chosenNumber = parseInt(enteredNumber);
+
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert('Invalid number!', 'Number has to be a number between 0 and 99.', [
+				{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }
+			]);
+			return;
+		}
+
+		onPickNumber(chosenNumber);
+	}
+
 	return (
 		<View style={styles.inputContainer}>
 			<TextInput
@@ -11,13 +35,15 @@ function StartGameScreen() {
 				keyboardType="number-pad"
 				autoCapitalize="none"
 				autoCorrect={false}
+				onChangeText={numberInputHandler}
+				value={enteredNumber}
 			/>
 			<View style={styles.buttonsContainer}>
 				<View style={styles.buttonContainer}>
-					<PrimaryButton>Reset</PrimaryButton>
+					<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
 				</View>
 				<View style={styles.buttonContainer}>
-					<PrimaryButton>Confirm</PrimaryButton>
+					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
 				</View>
 			</View>
 		</View>
