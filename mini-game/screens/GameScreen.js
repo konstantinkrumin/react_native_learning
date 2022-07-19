e8c4f9c342f54b8ebf1b6ad0e71631539d4480c7
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-07-19 02:37:35 -0400

    changes

--- mini-game/screens/GameScreen.js
+++ mini-game/screens/GameScreen.js
@@ -1,7 +1,22 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 function GameScreen() {
-	return <Text>Game Screen</Text>;
+	return (
+		<View style={styles.screen}>
+			<Text>Opponent's Guess</Text>
+			<View>
+				<Text>Higher or lower?</Text>
+			</View>
+			<View></View>
+		</View>
+	);
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 24
+	}
+});
