8cd22aa7d9e769bad373a8ad0dc71ef556626781
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-07-26 00:49:59 -0400

    setting up game over component

--- mini-game/screens/GameOverScreen.js
+++ mini-game/screens/GameOverScreen.js
@@ -1,7 +1,41 @@
-import { Text } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
+
+import Title from '../components/ui/Title';
+
+import Colors from '../constants/colors';
 
 function GameOverScreen() {
-	return <Text>Game is over!</Text>;
+	return (
+		<View style={styles.rootContainer}>
+			<Title>GAME OVER!</Title>
+			<View style={styles.imageContainer}>
+				<Image style={styles.image} source={require('../assets/images/success.png')} />
+			</View>
+			<Text>Your phone needed X rounds to guess the number Y.</Text>
+		</View>
+	);
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		flex: 1,
+		padding: 24,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: Colors.primary800,
+		overflow: 'hidden',
+		margin: 36
+	},
+	image: {
+		width: '100%',
+		height: '100%'
+	}
+});
