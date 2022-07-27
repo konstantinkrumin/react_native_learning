20a35820c48db57ddd1f09221b3040609fa03c6f
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-07-27 00:40:26 -0400

    customized status bar and added android and ios specific components

--- mini-game/components/ui/Title.ios.js
+++ mini-game/components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, Platform } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 
 function Title({ children }) {
 	return <Text style={styles.title}>{children}</Text>;
@@ -12,8 +12,6 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		color: 'white',
 		textAlign: 'center',
-		// borderWidth: Platform.OS === 'android' ? 2 : 0,
-		borderWidth: Platform.select({ ios: 0, android: 2 }),
 		borderColor: 'white',
 		padding: 12,
 		maxWidth: '80%',
