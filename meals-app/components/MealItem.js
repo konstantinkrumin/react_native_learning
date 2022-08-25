e8211d4289ec6830d36f9856e74e0051d2707f93
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-08-25 00:23:07 -0400

    parse data for Meal Detail Screen

--- meals-app/components/MealItem.js
+++ meals-app/components/MealItem.js
@@ -1,6 +1,8 @@
 import { View, Pressable, Text, Image, StyleSheet, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import MealDetails from './MealDetails';
+
 function MealItem({ id, title, imageUrl, duration, complexity, affordability }) {
 	const navigation = useNavigation();
 
@@ -22,11 +24,11 @@ function MealItem({ id, title, imageUrl, duration, complexity, affordability }) 
 						<Image source={{ uri: imageUrl }} style={styles.image} />
 						<Text style={styles.title}>{title}</Text>
 					</View>
-					<View style={styles.details}>
-						<Text style={styles.detailItem}>{duration}m</Text>
-						<Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-						<Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-					</View>
+					<MealDetails
+						duration={duration}
+						affordability={affordability}
+						complexity={complexity}
+					/>
 				</View>
 			</Pressable>
 		</View>
@@ -63,15 +65,5 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		fontSize: 18,
 		margin: 8
-	},
-	details: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center',
-		padding: 8
-	},
-	detailItem: {
-		marginHorizontal: 4,
-		fontSize: 12
 	}
 });
