ed24cd5fa95d8edba4bdb83ccf82e928d706dab2
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-08-25 01:00:54 -0400

    set up styling for Meal Detail page

--- meals-app/screens/MealDetailScreen.js
+++ meals-app/screens/MealDetailScreen.js
@@ -1,8 +1,10 @@
-import { Text, Image, View } from 'react-native';
+import { Text, Image, View, StyleSheet, ScrollView } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 
 import MealDetails from '../components/MealDetails';
+import Subtitle from '../components/MealDetail/Subtitle';
+import List from '../components/MealDetail/List';
 
 function MealDetailScreen({ route }) {
 	const mealId = route.params.mealId;
@@ -10,24 +12,52 @@ function MealDetailScreen({ route }) {
 	const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
 	return (
-		<View>
-			<Image source={{ uri: selectedMeal.imageUrl }} />
-			<Text>{selectedMeal.title}</Text>
+		<ScrollView style={styles.rootContainer}>
+			<Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+			<Text style={styles.title}>{selectedMeal.title}</Text>
 			<MealDetails
 				duration={selectedMeal.duration}
-				affordability={selectedMeal.affordability}
 				complexity={selectedMeal.complexity}
+				affordability={selectedMeal.affordability}
+				textStyle={styles.detailText}
 			/>
-			<Text>Ingredients</Text>
-			{selectedMeal.ingredients.map(ingredient => (
-				<Text key={ingredient}>{ingredient}</Text>
-			))}
-			<Text>Steps</Text>
-			{selectedMeal.steps.map(step => (
-				<Text key={step}>{step}</Text>
-			))}
-		</View>
+			<View style={styles.listOuterContainer}>
+				<View style={styles.listContainer}>
+					<Subtitle children="Ingredients" />
+					<List data={selectedMeal.ingredients} />
+
+					<Subtitle children="Steps" />
+					<List data={selectedMeal.steps} />
+				</View>
+			</View>
+		</ScrollView>
 	);
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+	rootContainer: {
+		marginBottom: 30
+	},
+	image: {
+		width: '100%',
+		height: 350
+	},
+	title: {
+		fontWeight: 'bold',
+		fontSize: 24,
+		margin: 8,
+		textAlign: 'center',
+		color: 'white'
+	},
+	detailText: {
+		color: 'white'
+	},
+	listOuterContainer: {
+		alignItems: 'center'
+	},
+	listContainer: {
+		maxWidth: '80%'
+	}
+});
