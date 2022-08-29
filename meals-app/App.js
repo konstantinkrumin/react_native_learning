5aab5b0910acb85063e5569decd5ff48621a78ca
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-08-28 23:37:58 -0400

    added styling to the drawer

--- meals-app/App.js
+++ meals-app/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import FavoritesScreen from './screens/FavoritesScreen';
 import CategoriesScreen from './screens/CategoriesScreen';
@@ -22,17 +23,34 @@ function DrawerNavigator() {
 				headerTintColor: 'white',
 				sceneContainerStyle: {
 					backgroundColor: '#3f2f25'
-				}
+				},
+				drawerContentStyle: {
+					backgroundColor: '#351401'
+				},
+				drawerInactiveTintColor: 'white',
+				drawerActiveTintColor: '#351401',
+				drawerActiveBackgroundColor: '#e4baa1'
 			}}
 		>
 			<Drawer.Screen
 				name="Categories"
 				component={CategoriesScreen}
 				options={{
-					title: 'All Categories'
+					title: 'All Categories',
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="list" color={color} size={size} />
+					)
 				}}
 			/>
-			<Drawer.Screen name="Favorites" component={FavoritesScreen} />
+			<Drawer.Screen
+				name="Favorites"
+				component={FavoritesScreen}
+				options={{
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="star" color={color} size={size} />
+					)
+				}}
+			/>
 		</Drawer.Navigator>
 	);
 }
