7f15b5090e01090c4a2949953c1b4d461222c409
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-08-28 22:46:14 -0400

    set up drawer navigation

--- other-navigators/App.js
+++ other-navigators/App.js
@@ -1,5 +1,48 @@
-import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: '#3c0a6b'
+					},
+					headerTintColor: 'white',
+					drawerActiveBackgroundColor: '#f0e1ff',
+					drawerActiveTintColor: '#3c0a6b'
+					// drawerStyle: {
+					// 	backgroundColor: '#ccc'
+					// }
+				}}
+			>
+				<Drawer.Screen
+					name="Welcome"
+					component={WelcomeScreen}
+					options={{
+						drawerLabel: 'Welcome!',
+						drawerIcon: ({ color, size }) => (
+							<Ionicons name="home" color={color} size={size} />
+						)
+					}}
+				/>
+				<Drawer.Screen
+					name="User"
+					component={UserScreen}
+					options={{
+						drawerIcon: ({ color, size }) => (
+							<Ionicons name="person" color={color} size={size} />
+						)
+					}}
+				/>
+			</Drawer.Navigator>
+		</NavigationContainer>
+	);
 }
--- other-navigators/screens/UserScreen.js
+++ other-navigators/screens/UserScreen.js
@@ -1,25 +1,30 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
 
-function UserScreen() {
-  return (
-    <View style={styles.rootContainer}>
-      <Text>
-        This is the <Text style={styles.highlight}>"User"</Text> screen!
-      </Text>
-    </View>
-  );
+function UserScreen({ navigation }) {
+	function openDrawerHandler() {
+		navigation.toggleDrawer();
+	}
+
+	return (
+		<View style={styles.rootContainer}>
+			<Text>
+				This is the <Text style={styles.highlight}>"User"</Text> screen!
+			</Text>
+			<Button title="Open Drawer" onPress={openDrawerHandler} />
+		</View>
+	);
 }
 
 export default UserScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  highlight: {
-    fontWeight: 'bold',
-    color: '#eb1064',
-  },
+	rootContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	highlight: {
+		fontWeight: 'bold',
+		color: '#eb1064'
+	}
 });
