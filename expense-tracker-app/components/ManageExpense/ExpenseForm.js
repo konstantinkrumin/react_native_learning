de1d567c769d34b7aa2474e41245ea37eaaa4304
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-09 16:53:46 -0400

    set up validation

--- expense-tracker-app/components/ManageExpense/ExpenseForm.js
+++ expense-tracker-app/components/ManageExpense/ExpenseForm.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Alert } from 'react-native';
 
 import Input from './Input';
 import Button from '../UI/Button';
@@ -28,6 +28,15 @@ function ExpenseForm({ submitButtonLabel, defaultValues, onCancel, onSubmit }) {
 			date: new Date(inputValues.date),
 			description: inputValues.description
 		};
+
+		const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+		const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+		const descriptionIsValid = expenseData.description.trim().length > 0;
+
+		if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+			Alert.alert('Invalid input', 'Please check your input values');
+			return;
+		}
 
 		onSubmit(expenseData);
 	}
