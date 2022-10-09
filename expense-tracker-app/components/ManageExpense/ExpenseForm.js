2bccc9a13af77ae34af846985346a3bf75111cd5
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-09 15:40:59 -0400

    set up layout for expense form

--- expense-tracker-app/components/ManageExpense/ExpenseForm.js
+++ expense-tracker-app/components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 import Input from './Input';
 
@@ -6,22 +6,28 @@ function ExpenseForm() {
 	function amountChangedHandler() {}
 
 	return (
-		<View>
-			<Input
-				label="Amount"
-				textInputConfig={{
-					keyboardType: 'decimal-pad',
-					onChangeText: amountChangedHandler
-				}}
-			/>
-			<Input
-				label="Date"
-				textInputConfig={{
-					placeholder: 'YYYY-MM-DD',
-					maxLength: 10,
-					onChangeText: () => {}
-				}}
-			/>
+		<View style={styles.form}>
+			<Text style={styles.title}>Your Expense</Text>
+			<View style={styles.inputsRow}>
+				<Input
+					label="Amount"
+					style={styles.rowInput}
+					textInputConfig={{
+						keyboardType: 'decimal-pad',
+						onChangeText: amountChangedHandler
+					}}
+				/>
+				<Input
+					label="Date"
+					style={styles.rowInput}
+					textInputConfig={{
+						placeholder: 'YYYY-MM-DD',
+						maxLength: 10,
+						onChangeText: () => {}
+					}}
+				/>
+			</View>
+
 			<Input
 				label="Description"
 				textInputConfig={{
@@ -33,3 +39,23 @@ function ExpenseForm() {
 }
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+	form: {
+		marginTop: 40
+	},
+	title: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		color: 'white',
+		marginVertical: 24,
+		textAlign: 'center'
+	},
+	inputsRow: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	rowInput: {
+		flex: 1
+	}
+});
--- expense-tracker-app/components/ManageExpense/Input.js
+++ expense-tracker-app/components/ManageExpense/Input.js
@@ -2,7 +2,7 @@ import { Text, TextInput, View, StyleSheet } from 'react-native';
 
 import { GlobalStyles } from '../../constants/styles';
 
-function Input({ label, textInputConfig }) {
+function Input({ label, style, textInputConfig }) {
 	const inputStyles = [styles.input];
 
 	if (textInputConfig && textInputConfig.multiline) {
@@ -10,7 +10,7 @@ function Input({ label, textInputConfig }) {
 	}
 
 	return (
-		<View style={styles.inputContainer}>
+		<View style={[styles.inputContainer, style]}>
 			<Text style={styles.label}>{label}</Text>
 			<TextInput style={inputStyles} {...textInputConfig} />
 		</View>
