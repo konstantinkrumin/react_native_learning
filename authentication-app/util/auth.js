8f49ccf0459c5a6314858a6f3659c702adad001e
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-25 23:55:09 -0400

    added login screen

--- authentication-app/util/auth.js
+++ authentication-app/util/auth.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 
 import { FIREBASE_API_KEY } from '@env';
 
+async function authenticate(mode, email, password) {
+	const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${FIREBASE_API_KEY}`;
+
+	const response = await axios.post(url, {
+		email: email,
+		password: password,
+		returnSecureToken: true
+	});
+
+	console.log(response.data);
+}
+
 export async function createUser(email, password) {
-	await axios.post(
-		'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + FIREBASE_API_KEY,
-		{
-			email: email,
-			password: password,
-			returnSecureToken: true
-		}
-	);
+	await authenticate('signUp', email, password);
+}
+
+export async function login(email, password) {
+	await authenticate('signInWithPassword', email, password);
 }
