7cdb4c89c935e5ab2026db1709a73d1d7af8dc5f
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-10-26 00:14:33 -0400

    set up connection between login and signup pages and context state

--- authentication-app/screens/LoginScreen.js
+++ authentication-app/screens/LoginScreen.js
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Alert } from 'react-native';
 
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
 import { login } from '../util/auth';
 
 function LoginScreen() {
 	const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+	const authCtx = useContext(AuthContext);
+
 	async function loginHandler({ email, password }) {
 		setIsAuthenticating(true);
 		try {
-			await login(email, password);
+			const token = await login(email, password);
+			authCtx.authenticate(token);
 		} catch (error) {
 			Alert.alert(
 				'Authentication failed!',
