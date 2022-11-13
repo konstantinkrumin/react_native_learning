bb47861721172a469c6236f0bb777181a41c6139
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-13 18:58:25 -0500

    set up add button with navigation in the header

--- favorite-places/App.js
+++ favorite-places/App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
+import IconButton from './components/UI/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,20 @@ export default function App() {
 			<StatusBar style="dark" />
 			<NavigationContainer>
 				<Stack.Navigator>
-					<Stack.Screen name="AllPlaces" component={AllPlaces} />
+					<Stack.Screen
+						name="AllPlaces"
+						component={AllPlaces}
+						options={({ navigation }) => ({
+							headerRight: ({ tintColor }) => (
+								<IconButton
+									icon="add"
+									size={24}
+									color={tintColor}
+									onPress={() => navigation.navigate('AddPlace')}
+								/>
+							)
+						})}
+					/>
 					<Stack.Screen name="AddPlace" component={AddPlace} />
 				</Stack.Navigator>
 			</NavigationContainer>
