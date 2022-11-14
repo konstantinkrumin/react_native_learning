2132fc218cf4604d9d4b30149256ac646ee999ce
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-14 00:07:28 -0500

    added location picker

--- favorite-places/components/Places/PlaceForm.js
+++ favorite-places/components/Places/PlaceForm.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native';
 
 import { Colors } from '../../constants/colors';
 import ImagePicker from './ImagePicker';
+import LocationPicker from './LocationPicker';
 
 function PlaceForm() {
 	const [enteredTitle, setEnteredTitle] = useState('');
@@ -22,6 +23,7 @@ function PlaceForm() {
 				/>
 			</View>
 			<ImagePicker />
+			<LocationPicker />
 		</ScrollView>
 	);
 }
