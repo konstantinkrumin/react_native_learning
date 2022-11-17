56e1d74b7c7d08dc346027b631cea7d64459e07c
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-17 01:04:13 -0500

    set up place obj passingh

--- favorite-places/components/Places/PlaceForm.js
+++ favorite-places/components/Places/PlaceForm.js
@@ -5,8 +5,9 @@ import Button from '../UI/Button';
 import ImagePicker from './ImagePicker';
 import LocationPicker from './LocationPicker';
 import { Colors } from '../../constants/colors';
+import { Place } from '../../models/place';
 
-function PlaceForm() {
+function PlaceForm({ onCreatePlace }) {
 	const [enteredTitle, setEnteredTitle] = useState('');
 	const [selectedImage, setSelectedImage] = useState();
 	const [pickedLocation, setPickedLocation] = useState();
@@ -24,9 +25,8 @@ function PlaceForm() {
 	}, []);
 
 	function savePlaceHandler() {
-		console.log(enteredTitle);
-		console.log(selectedImage);
-		console.log(pickedLocation);
+		const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+		onCreatePlace(placeData);
 	}
 
 	return (
