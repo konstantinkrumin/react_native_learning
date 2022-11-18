0a9d26acae7a69c1f5df3dff77df2a1e3d633182
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-18 01:19:49 -0500

    prep insert func

--- favorite-places/util/database.js
+++ favorite-places/util/database.js
@@ -27,3 +27,29 @@ export function init() {
 
 	return promise;
 }
+
+export function insertPlace(place) {
+	const promise = new Promise((resolve, reject) => {
+		database.transaction(tx => {
+			tx.executeSql(
+				`INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+				[
+					place.title,
+					place.imageUri,
+					place.address,
+					place.location.lat,
+					place.location.lng
+				],
+				(_, result) => {
+					console.log(result);
+					resolve(result);
+				},
+				(_, error) => {
+					reject(error);
+				}
+			);
+		});
+	});
+
+	return promise;
+}
