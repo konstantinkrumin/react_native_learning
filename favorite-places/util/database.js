78999dca09585e6dc7d21b594a602043070ce2e6
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-20 18:38:18 -0500

    set up readonly map

--- favorite-places/util/database.js
+++ favorite-places/util/database.js
@@ -97,7 +97,19 @@ export function fetchPlaceDetails(id) {
 				'SELECT * FROM places WHERE id = ?',
 				[id],
 				(_, result) => {
-					resolve(result.rows._array[0]);
+					const dbPlace = result.rows._array[0];
+					const place = new Place(
+						dbPlace.title,
+						dbPlace.imageUri,
+						{
+							lat: dbPlace.lat,
+							lng: dbPlace.lng,
+							address: dbPlace.address
+						},
+						dbPlace.id
+					);
+
+					resolve(place);
 				},
 				(_, error) => {
 					reject(error);
