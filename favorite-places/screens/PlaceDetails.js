70dd1f79ca88ff7dca3ff21f9469988ec76b1650
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-20 18:27:52 -0500

    set up place details fetching

--- favorite-places/screens/PlaceDetails.js
+++ favorite-places/screens/PlaceDetails.js
@@ -1,22 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import OutlinedButton from '../components/UI/OutlinedButton';
 import { Colors } from '../constants/colors';
+import { fetchPlaceDetails } from '../util/database';
 
-function PlaceDetails({ route }) {
+function PlaceDetails({ route, navigation }) {
+	const [fetchedPlace, setFetchedPlaces] = useState();
+
 	function showOnMapHandler() {}
 
 	const selectedPlaceId = route.params.placeId;
 
-	useEffect(() => {}, [selectedPlaceId]);
+	useEffect(() => {
+		async function loadPlaceData() {
+			const place = await fetchPlaceDetails(selectedPlaceId);
+			setFetchedPlaces(place);
+			navigation.setOptions({
+				title: place.title
+			});
+		}
+
+		loadPlaceData();
+	}, [selectedPlaceId]);
+
+	if (!fetchedPlace) {
+		return (
+			<View style={styles.fallback}>
+				<Text>Loading place data...</Text>
+			</View>
+		);
+	}
 
 	return (
 		<ScrollView>
-			<Image style={styles.image} />
+			<Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
 			<View style={styles.locationContainer}>
 				<View style={styles.addressContainer}>
-					<Text style={styles.address}>ADDRESS</Text>
+					<Text style={styles.address}>{fetchedPlace.address}</Text>
 				</View>
 			</View>
 			<OutlinedButton icon="map" onPress={showOnMapHandler}>
@@ -29,6 +50,11 @@ function PlaceDetails({ route }) {
 export default PlaceDetails;
 
 const styles = StyleSheet.create({
+	fallback: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
 	image: {
 		height: '35%',
 		minHeight: 300,
