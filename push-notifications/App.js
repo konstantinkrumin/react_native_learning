dbc9d2b711698a4e64120caf5be178d89223a2ea
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-11-25 00:35:21 -0500

    set a local notification to show

--- push-notifications/App.js
+++ push-notifications/App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => {
+		return {
+			shouldPlaySound: false,
+			shouldSetBadge: false,
+			shouldShowAlert: true
+		};
+	}
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	function scheduleNotificationHandler() {
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'My first local notification',
+				body: 'This is the body of the notification.',
+				data: {
+					userName: 'Costa'
+				}
+			},
+			trigger: {
+				seconds: 5
+			}
+		});
+	}
+
+	return (
+		<View style={styles.container}>
+			<Button title="Schedule Notification" onPress={scheduleNotificationHandler} />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
